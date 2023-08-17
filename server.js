f31a46874b7df234b8443227c68d0e1ba963c122
Mahesh Jangid <mesh.jangid@gmail.com>
2023-08-18 00:39:32 +0530

    changes

--- server.js
+++ server.js
@@ -31,6 +31,7 @@ app.use(
 app.use("/api/users", userRoutes);
 app.use("/api/employee", employeeRoutes);
 
+const __dirname = path.resolve();
 app.use(express.static(path.join(__dirname, 'dist/')));
 
 // Serve index.html for all routes
